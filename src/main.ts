c56f8f1960fe43f8128017b411158deb36151f3a
Shreeyash Shrestha <shresthashree0807@gmail.com>
2025-02-02 16:36:30 +0545

    Update enable cors function

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,13 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    // FIXME use env variables for origin
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
   console.log('App is running on port:', process.env.PORT ?? 8000);
   await app.listen(process.env.PORT ?? 8000);
 }
